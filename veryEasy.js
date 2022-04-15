6122d2f402e339c9064924507d31b717fdba11b0
hdyehy <trigo.matthew@outlook.com>
2022-04-15 08:21:27 -0500

    revised veryHard.js to have functions

--- veryEasy.js
+++ veryEasy.js
@@ -2,4 +2,4 @@ const x = 76;
 const y = 60;
 let diff = x - y; //calculates difference of x and y
 
-console.log(`the difference between ${x} and ${y} is ${diff}.`) //tells difference of x and y in a sentence.
+console.log(`the difference between ${x} and ${y} is ${diff}.`); //tells difference of x and y in a sentence.
--- veryHard.js
+++ veryHard.js
@@ -1,24 +1,45 @@
 
 //prompts user for the numbers and operator they want to calculate
-let x = prompt("Welcome to my calculator! please enter your first number: ")
-let op = prompt(" Now choose an operator (+, -, /, *) : ")
-let y = prompt("Finally, enter your second number: ")
+let x = prompt("Welcome to my calculator! please enter your first number: ");
+let op = prompt(" Now choose an operator (+, -, /, *) : ");
+let y = prompt("Finally, enter your second number: ");
 
+x = parseFloat(x);
+y = parseFloat(y);
+//adds two numbers
+function add(n0, n1) {
+    return n0 + n1
+}
+
+//subtracts two numbers
+function subtract(n0, n1) {
+    return n0 - n1;
+}
+
+//multiplies two numbers
+function multiply(n0, n1) {
+    return n0 * n1;
+}
+
+//divides two numbers
+function divide(n0, n1) {
+    return n0 / n1;
+}
 //calculates the results based on the operator and displays those results
 
 if (op == '+') {
-    let calc = x + y
-    alert(`${x} + ${y} = ${calc}`)
+
+    alert(`${x} + ${y} = ${add(x, y)}`);
 
 } else if (op == '-') {
-    let calc = x - y
-    alert(`${x} - ${y} = ${calc}`)
+
+    alert(`${x} - ${y} = ${subtract(x, y)}`);
 
 } else if (op == '/') {
-    let calc = x / y
-    alert(`${x} / ${y} = ${calc}`)
+
+    alert(`${x} / ${y} = ${divide(x, y)}`);
 
 } else if (op == '*') {
-    let calc = x * y
-    alert(`${x} * ${y} = ${calc}`)
+
+    alert(`${x} * ${y} = ${multiply(x, y)}`);
 }
